3b2b637ee6f5f904612bd07acd2aa74f8fd7241f
JayLaxami <juhis14292@gmail.com>
2023-04-11 22:01:07 -0500

    second

---
+++ data/vaccine.sql
@@ -0,0 +1,22 @@
+DROP TABLE vaccine
+CREATE TABLE vaccine (
+	id INT NOT NULL,
+	date date NOT NULL,
+	location VARCHAR NOT NULL,
+	total_vaccinations FLOAT NOT NULL ,
+	total_vaccinations_per_hundred FLOAT NOT NULL,
+	total_distributed FLOAT NOT NULL,
+	distributed_per_hundred FLOAT NOT NULL,
+	people_vaccinated FLOAT NOT NULL,
+	people_vaccinated_per_hundred FLOAT NOT NULL,
+	people_fully_vaccinated FLOAT NOT NULL,
+	people_fully_vaccinated_per_hundred FLOAT NOT NULL,
+	daily_vaccinations FLOAT NOT NULL,
+	daily_vaccinations_per_million FLOAT NOT NULL,
+	total_boosters FLOAT NOT NULL,
+	total_boosters_per_hundred FLOAT NOT NULL
+ 
+);
+ SELECT *
+ FROM vaccine
+ 
